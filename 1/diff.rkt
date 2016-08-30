0b56477e9bfeaf2173159ff2da69458872835a84
Sourec <crmyers@wpi.edu>
2016-08-29 20:58:01 -0400

    Add insert function
    Insert function works with strings by taking a number, a string, and
    a patch to apply.

--- 1/diff.rkt
+++ 1/diff.rkt
@@ -6,4 +6,19 @@
 ; opcode, symbol; position, number; data, string
 (define-struct operation (opcode position data))
 
+; apply-op, takes an op and produces a patched string
+; op -> string
+; TODO: Test cases
+;(define (apply-op op str)
+;	()) ; TODO: Implement
+
+
+; insert, inserts a substring at a position
+; number string string -> string
+(check-expect (insert 4 "The fox" "quick brown " ) "The quick brown fox") ; Can't use these with DrRacket; commented for now
+(define (insert position str patch)
+	(string-append 
+	(substring str 0 position) 						; part 1: get the first half
+	patch									; part 2: just tack on the inserted part
+	(substring str position (string-length str))))				; part 3: (tricky part) tack on the end of the string
 
