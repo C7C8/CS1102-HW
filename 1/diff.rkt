fa29616d4ce75dd3f607ca84b19081871148b750
Sourec <crmyers@wpi.edu>
2016-08-30 12:12:46 -0400

    Add apply-patch function, fix some docs, delete mysterious "diff.2" file
    -There's an apply-patch function now! It takes a patch and... well,
     applies it!
    -Documentation was fixed for some lines, bringing it all up to the
     class standard of ;; for a comment line. The docs that needed
     fixing were mostly mine... whoops!
    -Mysterious file "diff.2", apparently saved by DrRacket, is now
     banished to the outer regions of /dev/null. I have no idea where it
     came from, the best I can think of is that DrRacket was so kind as to
     bestow it upon us. IDEs work in mysterious ways...

--- 1/diff.rkt
+++ 1/diff.rkt
@@ -1,31 +1,46 @@
 #!/usr/bin/racket
-#lang racket
+#lang racket				;; These three lines are required to run the program without DrRacket
+(require test-engine/racket-tests)
+				
+
+;; patch, contains data needed to apply an patch
+;; opcode, symbol (`ADD, 'DEL); str, string; pos1, number; pos2, number.
+;; Intended to be used in the apply-patch function.
+(define-struct patch (opcode pos str len))
+
+;; Examples:
+(define del-patch (make-patch 'DEL 0 "" 3))
+(define add-patch (make-patch 'ADD 4 "quick " 5))
 
 
-; Operation, contains data about... well, an operation!
-; opcode, symbol; position, number; data, string
-(define-struct operation (opcode position data))
+;; apply-patch: patch string -> string
+;; Consumes an patch (above) and a string, returns a patched string
+(check-expect (apply-patch del-patch "The quick brown fox") " quick brown fox")
+(check-expect (apply-patch add-patch "The brown fox") "The quick brown fox")
+(define (apply-patch patch str)
+	(cond
+		[(symbol=? 'ADD (patch-opcode patch))
+			(insert (patch-pos patch) str (patch-str patch))]
+		[(symbol=? 'DEL (patch-opcode patch))
+			(delete (patch-pos patch) (patch-len patch) str)]))
 
-; apply-op, takes an op and produces a patched string
-; op -> string
-; TODO: Test cases
-;(define (apply-op op str)
-;	()) ; TODO: Implement
 
-;;Delete: number number string -> string
-;;Consumes a starting position, an amount to delete and the string to delete from
-;;Produces a string with specified deleted portion via starting position and deleted amount
+;; Delete: number number string -> string
+;; Consumes a starting position, an amount to delete and the string to delete from
+;; Produces a string with specified deleted portion via starting position and deleted amount
 (check-expect (delete 2 2 "apples") "apes")
 (check-expect (delete 1 5 "0123456") "06")
 (define (delete position num str)
 	(string-append (substring str 0 position) (substring str (+ position num))))
 
-; insert, inserts a substring at a position
-; number string string -> string
-(check-expect (insert 4 "The fox" "quick brown " ) "The quick brown fox") ; Can't use these with DrRacket; commented for now
-(define (insert position str patch)
-	(string-append 
-	(substring str 0 position) 						; part 1: get the first half
-	patch									; part 2: just tack on the inserted part
-	(substring str position (string-length str))))				; part 3: (tricky part) tack on the end of the string
 
+;; number string string -> string
+;; Consumes a position, a string, and a substring. Inserts a substring at a position in the string.
+(check-expect (insert 4 "The fox" "quick brown " ) "The quick brown fox")
+(define (insert position str substr)
+	(string-append 
+	(substring str 0 position)
+	substr
+	(substring str position (string-length str))))
+
+(test) ;; Run check-expect tests. Not needed if DrRacket is configured right.
