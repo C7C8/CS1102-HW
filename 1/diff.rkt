a177731c9280d221f131b67c3a6105f153863102
Mike <mjkrebs@wpi.edu>
2016-09-01 17:02:23 -0400

    Problems 7-12

--- 1/diff.rkt
+++ 1/diff.rkt
@@ -1,5 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname diff) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
 #!/usr/bin/racket
-#lang racket				;; These three lines are required to run the program without DrRacket
+;;#lang racket				;; These three lines are required to run the program without DrRacket
 (require test-engine/racket-tests)
 
 ;; A delete is (make-delete number)
@@ -45,7 +48,7 @@
 
 
 ;; apply-patch: patch string -> string
-;; Consumes an patch and a string, returns a patched string
+;; Consumes a patch and a string, returns a patched string
 (check-expect (apply-patch patch-ins-ex "We are not programmers.") "We are not lazy programmers.")
 (check-expect (apply-patch patch-del-ex "I don't have other example sentences.") "I don't have example sentences.")
 (define (apply-patch patch str)
@@ -112,7 +115,7 @@
 
 ;; modernize: string -> string
 ;; Consumes a string and applies a series of patches to modernize it. Intended to
-;; operate on either a sequence from "Hamlet"
+;; operate on the sequence from "Hamlet"
 (check-expect (modernize hamlet-str) hamlet-str-modern)
 (define (modernize str)
 	(patch-walker modernize-patchlist str))
@@ -214,5 +217,55 @@
 	substr
 	(substring str position (string-length str))))
 
-(test) ;; Run check-expect tests. Not needed if DrRacket is configured in "Beginning Student" mode.
+;;(test) ;; Run check-expect tests. Not needed if DrRacket is configured in "Beginning Student" mode.
 
+#|
+  7.The advantages to returning false instead of the original string in the case where
+there is an overlap of two patches is that immediately one can know that the patches overlapped
+and didn't instead have a minor error like an index issue. This saves the coder the
+time of searching through the returned string looking for a patch that failed to be
+performed correctly when none of the patches were performed at all. 
+
+============
+Evaluations:
+============
+8.(/ (- (* 9 3) (double 'a)) 2) where double is defined as (define (double n) (* n 2))
+
+=(/ (- (27) (double 'a)) 2)
+
+=(/ (- (27) (* a' 2)) 2)
+
+= Error: double n suspected a number but what given a'. They cannot be multiplied together
+
+9.(or (< 5 2) (and (= 15 (- 18 3)) (> 8 4)))
+
+=(or (false) (and (= 15 (- 18 3)) (> 8 4)))
+
+=(or (false) (and (= 15 (15)) (> 8 4)))
+
+=(or (false) (and (true) (> 8 4)))
+
+=(or (false) (and (true) (true)))
+
+=(or (false) (true))
+
+= true
+
+
+10.(and (+ 5 -1) false)
+
+=(and (4) false)
+
+= Error: and returns a boolean and since 4 is neither an and or else the and cannot be evaluated
+
+11.(apply-patch 'remove "this is a test string") [use your own apply-patch program from this assignment]
+
+= Error: apply-patch requires (patch string) as arguments but was given (symbol string).
+    Our program doesn't use symbols to create an operation, only make-insert and make-delete do.
+
+===============
+Error Messages:
+===============
+
+
+|#
