e8e7676bf1753cfab053f10ef6ca3d457b36909b
Sourec <crmyers@wpi.edu>
2016-09-03 09:08:26 -0400

    Add simple graphics test; displays plane on screen

--- 2-FireGame/firegame.cpp
+++ 2-FireGame/firegame.cpp
@@ -2,9 +2,33 @@
 using namespace std; //ugh
 using namespace Hydra;
 
+#define WSIZE_X 800
+#define WSIZE_Y 600
+
 int main (int argc, char* argv[])
 {
-	cout << "Hello world!" << endl;
-	return 0;
+	HydraEngine* engine = HydraEngine::getInstance();
+	TextureManager* tmanage = TextureManager::getInstance();
+	engine->init();
+	engine->setWSize(WSIZE_X, WSIZE_Y);
+	engine->setWTitle("FireGame v1.0-cpp");
+
+	//Load textures
+	tmanage->loadTexture("plane.jpg", "plane");
+	tmanage->loadTexture("lg-fire.gif", "lg-fire");
+	tmanage->loadTexture("med-fire.gif", "med-fire");
+	tmanage->loadTexture("sm-fire.gif", "sm-fire");
+	Texture plane = tmanage->getTexture("plane");
+	plane.setX(WSIZE_X / 2);
+	plane.setY(WSIZE_Y / 2);
+
+	Timer pauseTimer(5000); //ms?
+	engine->queueTexture(plane);
+	engine->renderAll();
+	pauseTimer.start();
+	while (!pauseTimer.hasIntervalPassed());
+	
+	
+	engine->shutdown();
 }
 
