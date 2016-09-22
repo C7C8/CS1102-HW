b19e8dc0d1010da1ac09eb4b31a12568e16866d6
Sourec <crmyers@wpi.edu>
2016-09-22 18:21:15 -0400

    Add early prototype of imperative exam language

--- 5-Testing_Language/hwk5.rkt
+++ 5-Testing_Language/hwk5.rkt
@@ -6,4 +6,29 @@
 ;; =========================================
 
 
+;; A question is (make-question string string number)
+;; "Limit" is the minimum percentage needed for the student to be
+;; asked this question.
+(define-struct question (question answer limit))
+
+
+;; A section is (make-section string list[question])
+(define-struct section (name questions))
+
+
+;; A cmd is either
+;; -a symbol,
+;; -(make-PRINT string),
+;; -(make-IF-RESULTS number cmd cmd),
+;; -(make-GIVE-SECTION list[section])
+;; -(make-NOP)						;;No-Operation -- Only used for empty cases in IF statements.
+(define PRINT-SECTION-RESULTS 	'psr)
+(define PRINT-EXAM-RESULTS 	'per)
+(define-struct PRINT (msg))				
+(define-struct IF-RESULTS (bounds ctrue cfalse))	;;the student's current score is compared to bounds. If less than bounds, ctrue; else, cfalse.
+(define-struct GIVE-SECTION questions)
+(define-struct NOP)
+
+
+;; An exam is a list[cmd]
 
