3d49ecfe3f31f6bb93fa265355dec0195622331b
Mike <mjkrebs@wpi.edu>
2016-09-29 14:22:14 -0400

    Questions 1 and 2 but we can revise the answers they're not set in stone

---
+++ Questions-HW6/HW6.rkt
@@ -0,0 +1,70 @@
+;;===========================================
+;; || Christopher Myers and Michael Krebs ||
+;;===========================================
+
+#|
+____________________
+
+ QUESTION 1
+____________________
+
+PART A)
+   Three features of Lisp that Graham believes gave
+his company a competitive advantage were one lisp is a
+good language for writing software quickly, two lisp
+has the most powerful macros because it has "no syntax"
+and three Lisp is so high level that they didnt have to
+hire a big development team which saved them money
+
+PART B)
+   The two observations that we found to be the most interesting
+were the Blub paradox and that computer hardware changes so much
+faster than personal habits.
+   The Blub paradox is when a programmer
+is devoted to a certain langauge so much so that they think all
+languages are either not worth learning because they are satisfied
+with their langauge or too simplistic because it does not support
+features that their langauge does. Essentially they can't imagine
+a language more powerful than their own because the more powerful
+languages aren't considered to be more powerful just different and
+the less powerful languages to be too simplistic. We found this
+observation to be significant because it displays the foolishness
+of being monolingual when it comes to programming.
+   Graham's observation that computer hardware changes so much
+faster than personal habits means that the full capabilities of a
+new language or hardware is never implemented until many years later.
+And this is because people are unwilling or incapable of switching
+their habits to the new capabilities. We found this to be a
+significant observation because it stresses the abiility to adapt
+being a programmer, it is similar to how being monolingual is
+foolish but instead focuses more on the premise of being unable
+to change and mold as a programmer is foolish.
+
+____________________
+
+ QUESTION 2
+____________________
+
+ Three questions that Graham teaches to ask when approaching a new
+language are "who is this programmed designed for?" "how many people
+were behind its creation?" and "why are people using it?"
+If the answer to these questions are "for large organizations and thus
+best for a wide range of skilled programmers," "more than a small group
+who developed the language for their own use" and "because their bosses
+are telling them to or they hear its great from the press" respectively
+than the langauge is likely to not be successful. Good langauges are
+instead limitless to the programmer and not designed to be easy. They
+should be made because it would better serve the creators: had function
+and finally it should be used because programmers love it not because
+they are forced to use it or influenced to.
+
+____________________
+
+ QUESTION 3
+____________________
+
+
+
+
+
+|#
