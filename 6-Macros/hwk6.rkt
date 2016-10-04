83f225ab014c2cb7cce03b18403911ffbaad8551
Sourec <crmyers@wpi.edu>
2016-10-04 19:04:29 -0400

    Finish part 1

--- 6-Macros/hwk6.rkt
+++ 6-Macros/hwk6.rkt
@@ -1,51 +1,14 @@
 #!/usr/bin/racket
 (require test-engine/racket-tests)
 
-#|
-Original (functional) code:
-	
-	;; A dillo is a (make-dillo number boolean)
-	(define-struct dillo (length dead?))
-	
-	;; longer-than? : dillo number -> boolean
-	;; is dillo longer than given length
-	(define (longer-than? adillo len)
-	  (> (dillo-length adillo) len))
-	
-	;; run-over : dillo -> dillo
-	;; return dead dillo one unit longer than given dillo
-	(define (run-over adillo)
-      (make-dillo (+ (dillo-length adillo) 1) true))
-	
-Pre-macro OO code:
+;; Michael Krebs and Christopher Myers
 
-    (define make-dillo-obj
-      (lambda (length dead?)
-        (lambda (message)
-          (cond [(symbol=? message 'longer-than?) 
-                 (lambda (len) (> length len))]
-                [(symbol=? message 'run-over) 
-                 (lambda () (make-dillo-obj (+ length 1) true))]))))
 
-    (define d1 (make-dillo-obj 5 false))
-    ((d1 'longer-than?) 6)
-    ((d1 'longer-than?) 5)
-    (define d2 ((d1 'run-over)))
-    ((d2 'longer-than?) 5)
 
-Post-macro OO code:
-|#
+;; ==========
+;; | PART I |
+;; ==========
 
-#|(define dillo-class
-    (class (initvars length dead?)
-      (method longer-than? (len) (> length len))
-      (method run-over () (dillo-class (+ length 1) true))))
-
-(define d3 (dillo-class 5 false))
-(send d3 longer-than? 6)
-(send d3 longer-than? 5)
-(define d4 (send d3 run-over))
-(send d4 longer-than? 5)|#
 
 (define-syntax class
   (syntax-rules ()
@@ -55,12 +18,11 @@ Post-macro OO code:
        (lambda (message)
          (cond [(symbol=? message 'mname) ; M'Function *tips parameter*
                 (lambda (mparam ...) mfunc)] ...)))]))
-
 (define-syntax send
   (syntax-rules ()
-    [(send obj funcname params)
+    [(send obj funcname params ...)
+     ((obj 'funcname) params ...)]))
 
-     ((obj 'funcname) params)]))
 
 (define dillo-class
   (class (initvars length dead?)
@@ -69,4 +31,8 @@ Post-macro OO code:
 
 (define d3 (dillo-class 5 false))
 (send d3 longer-than? 6)
-  
+(send d3 longer-than? 5)
+(define d4 (send d3 run-over))
+(send d4 longer-than? 5)
+
+ 
