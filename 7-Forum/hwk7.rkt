212378b2259b19efaf10f940618ad87771135c0c
Sourec <crmyers@wpi.edu>
2016-10-10 19:51:35 -0400

    Add rendering of forum posts... it's pretty stupid

--- 7-Forum/hwk7.rkt
+++ 7-Forum/hwk7.rkt
@@ -1,16 +1,52 @@
 (load "server.rkt")
+(require test-engine/racket-tests)
+(require racket/string)
 
-(define-script (main form cookies)
-  (values
-   (html-page "Forum"
-              (list 'h1 "CS 1102 Forum")
-              (format "Where CS students go to learn")
+;; Christopher Myers and Michael Krebs
+
+
+;; ======================
+;; || POST FUNCTIONS ||
+;; ======================
+
+;; A post is (make-post string string string)
+(define-struct post (author title body))
+
+;; list[post]
+(define mem (list (make-post "crmyers" "Test post" "Test content")
+                  (make-post "mjkrebs" "Test pos2" "Test conten2")
+                  (make-post "dillo" "[no title]" "DEAD")))
+
+(define (add-post npost)
+  (set! mem (cons npost mem)))
+
+
+;; post->string: post -> string
+;; Does what it says on the box.
+(check-expect (post->string (make-post "crmyers" "Test post" "Test content"))
+              "crmyers      Test post      Test content<br/>")
+(define (post->string pst)
+  (string-append (post-author pst) "      " (post-title pst) "      " (post-body pst) "<br/>"))
+
+;; posts->string: list[post] -> string
+;; Also does what it says on the box. Turns a list of posts into strings!
+(check-expect (posts->string (list (make-post "crmyers" "Test post" "Test content")
+                             (make-post "mjkrebs" "Test pos2" "Test conten2")))
+  "<p>crmyers      Test post      Test content<br/>mjkrebs      Test pos2      Test conten2<br/></p>")
+(define (posts->string pstlst)
+  (string-append "<p>" (string-append* (map post->string pstlst)) "</p>"))
+
+;; =================
+;; || WEB SCRIPTS || 
+;; =================
+
+(define-script (main form cookies) ; args ignored... somehow
+  (values 
+   (html-page "Main Page"
+              (list 'h1 "Racket Forum")
+              "Author         Title          Body"
               (list 'br)
-              (list 'br)
-              (append (list 'form 
-                            (list (list 'action "http://localhost:8088/author")
-                                  (list 'target "_blank")))
-                      (list(list 'input (list (list 'type "submit") (list 'value "Add New Post"))))))
+              (string->xexpr (posts->string mem)))
    false))
 
 (define-script (author form cookies)
@@ -46,3 +82,6 @@
                                   (list 'target "_blank")))
                         (list (list 'input (list(list 'type "submit") (list 'value "Submit"))))))
    false))|#
+
+
+(test)
